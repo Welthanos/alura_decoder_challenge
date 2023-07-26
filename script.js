2c7a3ccff64a7b0fd7c3083e8e18a04852fe214b
Welthanos <welthanos_dev@outlook.com>
2023-07-26 02:50:47 -0300

    feat: text being rendered in the result container

--- script.js
+++ script.js
@@ -6,28 +6,59 @@ const secretWords = {
     "u": "ufat"
 };
 
-const encryptedText = [];
-const decryptedText = [];
+const reversedSecretWords = {};
+
+// Localiza as <div>s opcionais do container de resultado
+const emptyDiv = document.getElementById("empty");
+const filledDiv = document.getElementById("filled");
+
+// Inverte a ordem de chave e valor de secretWords e armazena em resversedSecretsWords
+function reverseSecretWords() {
+
+    for (const key in secretWords) {
+        const value = secretWords[key];
+        reversedSecretWords[value] = key;
+    }
+}
+
 
 function encrypt() {
     let text = document.getElementById("text").value;
 
     for (const [key, value] of Object.entries(secretWords)) {
-        text = text.replace(key, value);
+
+        // Dá o replace de forma global (todos os elementos)
+        text = text.replace(new RegExp(key, 'g'), value);
     }
 
-    encryptedText.unshift(text);
+    const encryptedText = document.getElementById("text-result");
+
+    // Renderiza o texto criptografado na <div> de resultado
+    encryptedText.innerHTML = text;
+
+    // Esconde a div principal e renderiza a div com o texto criptografado
+    emptyDiv.style.display = "none";
+    filledDiv.style.display = "block";
 }
 
 function decrypt() {
     let text = document.getElementById("text").value;
 
-    for (const [key, value] of Object.entries(secretWords)) {
-        text = text.replace(value, key);
+    // Inverte a ordem de chave e valor para descriptografar
+    reverseSecretWords();
+
+    for (const [key, value] of Object.entries(reversedSecretWords)) {
+
+        // Dá o replace de forma global (todos os elementos)
+        text = text.replace(new RegExp(key, 'g'), value);
     }
 
-    decryptedText.unshift(text);
-}
+    const decryptedText = document.getElementById("text-result");
 
-console.log(encryptedText);
-console.log(decryptedText);
+    // Renderiza o texto descriptografado na <div> de resultado
+    decryptedText.innerHTML = text;
+
+    // Esconde a div principal e renderiza a div com o texto descriptografado
+    emptyDiv.style.display = "none";
+    filledDiv.style.display = "block";
+}
